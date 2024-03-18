88848e2e36a0cad4e49fad9fc594051cc67225e2
sriram-raghavan <155535490+sriram-raghavan@users.noreply.github.com>
2024-03-17 20:21:24 -0400

    Update validateNamespace.sh

--- cluster/validateNamespace.sh
+++ cluster/validateNamespace.sh
@@ -6,7 +6,7 @@ jq="C:\Users\sriram\Downloads\jq.exe"
 # Function to validate namespace
 validate_namespace() {
     namespace="$1"
-    
+
     # Check if the namespace exists
     namespace_exists=$(kubectl get namespaces -o json | $jq -r --arg ns "$namespace" '.items[] | select(.metadata.name == $ns) | .metadata.name')
 
@@ -14,7 +14,7 @@ validate_namespace() {
         echo "Namespace '$namespace' does not exist."
         return 1
     else
-        echo "Namespace '$namespace' exists."        
+        # echo "Namespace '$namespace' exists."        
         return 0
     fi
 }
