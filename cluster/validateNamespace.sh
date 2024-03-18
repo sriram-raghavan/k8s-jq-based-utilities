c9d92a373d79d92324552a5a3e3e6fd39aaec158
sriram-raghavan <155535490+sriram-raghavan@users.noreply.github.com>
2024-03-17 20:53:37 -0400

    Update validateNamespace.sh
    Moving kubectl check, current context check to validate namespace logic.

--- cluster/validateNamespace.sh
+++ cluster/validateNamespace.sh
@@ -7,14 +7,32 @@ jq="C:\Users\sriram\Downloads\jq.exe"
 validate_namespace() {
     namespace="$1"
 
+    # Check if kubectl is installed
+    if ! command -v kubectl &> /dev/null; then
+        echo "Error: kubectl is not installed. Please install kubectl and try again."
+        exit 1
+    fi
+
+    # Get the current context
+    current_context=$(kubectl config current-context)
+
+    # Check if the current context is empty
+    if [ -z "$current_context" ]; then
+        echo "Error: No current context found. Make sure your kubeconfig is properly configured."
+        exit 1
+    fi
+
+    # Print the current context
+    echo "Current context: $current_context"
+
     # Check if the namespace exists
     namespace_exists=$(kubectl get namespaces -o json | $jq -r --arg ns "$namespace" '.items[] | select(.metadata.name == $ns) | .metadata.name')
 
     if [ -z "$namespace_exists" ]; then
-        echo "Namespace '$namespace' does not exist."
+        echo "Namespace '$namespace' does not exist in $current_context."
         return 1
     else
-        # echo "Namespace '$namespace' exists."        
+        echo "Namespace '$namespace' exists in $current_context."        
         return 0
     fi
 }
